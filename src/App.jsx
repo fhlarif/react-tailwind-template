1bb21e7789bb56a8f9bbf5bc596834c2bcbd5a9c
fathularif <fathulcode@gmail.com>
2022-08-03 20:38:01 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen min-w-full text-center flex justify-center bg-slate-800 text-slate-200">
+      <span className=" text-lg capitalize self-center">This is a starter template for Vite + React + Tailwind</span>
+    </div>
+  );
+}
+
+export default App;
